12b4b77f9282a9b71eca17fd85d8a3cdd777b561
Faisal Ahmed <thephpx@gmail.com>
2018-02-23 21:50:39 +0600

    base configuration setup done

---
+++ inpsyde.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:     Inpsyde
+ * Plugin URI:      http://www.github.com/thephpx/inpsyde
+ * Description:     Inpsyde demo plguin
+ * Author:          Faisal Ahmed
+ * Author URI:      http://www.faisalbd.com/
+ * Text Domain:     inpsyde
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Inpsyde
+ */
+
+
+
